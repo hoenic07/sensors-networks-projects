7343854cca16c2ec3708110888def15ea4b62a70
Niklas Hoesl <nih@runtastic.com>
2016-04-25 18:40:10 +0200

    REM unused code

--- project1/main/Instance.cpp
+++
@@ -1,9 +0,0 @@
-#include "Instance.h"
-/*Accelerometer* Instance::accelerometer = new Accelerometer();
-Monitor* Instance::monitor = new Monitor();
-Parameters* Instance::parameters = new Parameters();
-Thermometer* Instance::thermometer = new Thermometer();
-LED* Instance::led = new LED();
-Bus* Instance::bus = new Bus();
-Display* Instance::display = new Display();
-Input* Instance::input = new Input();*/
