4671cbb6f3cb8bc6be2593b01f2f24e4bf04ca57
Magda <magda.radinger@gmail.com>
2016-06-10 17:22:14 +0200

    MOD code cleanup and bug fix

--- project2/bagback/ContextSituation.h
+++ project2/bagback/ContextSituation.h
@@ -12,22 +12,24 @@ public:
   ~ContextSituation();
   void update();
   void setAcc(double x, double y, double z);
-  void setTemperature(double t);
   void setLumen(double l);
 private:
-  double temp;
+  double accX;
+  double accY;
+  double accZ;
   double lumen;
   Piezo* piezo;
   Led* standingLed;
   Led* walkingLed;
   Led* runningLed;
   RGBLed* rgbLed;
-
+  AccelerometerAnalyzer* accAnalyzer;
+  
   int discoCnt = 0;
   const int DISCO_CNT_MAX = 3;
   
   const int THRESHOLD_OPEN = 500;
-  AccelerometerAnalyzer* accAnalyzer;
+  
   const int LED_ID_STANDING = 7;
   const int LED_ID_WALKING = 6;
   const int LED_ID_RUNNING = 5;
@@ -36,6 +38,11 @@ private:
   const int LED_ID_BLUE = 9;
 
   void applyDiscoMode(boolean active);
+  const int RINGBUFFER_SIZE = 40;
+  double totalAccRingBuffer[40];
+  int ringBufferPos=0;
+  double ringBufferSum=0;
+  double windowedAcceleration=0;
 };
 
 #endif
