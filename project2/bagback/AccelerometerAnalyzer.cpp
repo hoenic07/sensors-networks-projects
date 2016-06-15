bf7d0d78ab92f26b285e9bf198f901febcae5c5a
Niklas Hoesl <nih@runtastic.com>
2016-06-15 08:57:46 +0200

    MOD acc

--- project2/bagback/AccelerometerAnalyzer.cpp
+++ project2/bagback/AccelerometerAnalyzer.cpp
@@ -3,8 +3,7 @@
 AccelerometerAnalyzer::AccelerometerAnalyzer(){
   rawBuffer = new RingBuffer(5);
   smoothedBuffer = new RingBuffer(50);
-  stepsWindow = new RingBuffer(100);
-  //stepsWindow->showDebug=1;
+  stepsWindow = new RingBuffer(4);
 }
 
 AccelerometerAnalyzer::~AccelerometerAnalyzer(){
@@ -42,25 +41,46 @@ void AccelerometerAnalyzer::update(double sample){
         }
   }
 
-  stepsWindow->add(isStep);
+  int lastStepTimeAgo=0;
+  int stepInsert = -1;
 
+  //Debug
+  if(isStep==1){
+    Serial.print(lastStepTime-sampleCount);
+    Serial.print(";");
+    Serial.println(smoothedSample);
+  }
+
+  // Step frequency
+  if(isStep==1 && lastStepTime != 0){
+    stepInsert = abs(lastStepTime-sampleCount);
+    lastStepTime=sampleCount;
+  }
+  else if(isStep==1&&lastStepTime==0){
+    lastStepTime=sampleCount;
+    stepInsert=lastStepTime;
+  }
+  else if(isStep==0){
+    lastStepTimeAgo = abs(lastStepTime-sampleCount);
+  }
+
+  //insert the stepTime into the window
+  if(stepInsert != -1){
+    if(stepInsert > 25) stepInsert = 25;
+    stepsWindow->add(stepInsert);
+  }
+  
   //sum of steps within last 10 seconds
-  int sumSteps = stepsWindow->getSum(); 
+  int stepFreq = stepsWindow->getAverage(); 
 
-  //Serial.print(sumSteps);
-
-  if(sumSteps < 2){
+  // 1 step = 1/20 sec
+  if(lastStepTimeAgo >= 25 || stepFreq == -1){
     state = 0;
-    //Serial.println(" Standing");
+    stepsWindow->clear();
   }
-  else if(sumSteps < 8){
-    state  = 1;
-    //Serial.println(" Walking");
-  }
-  else if(sumSteps >= 8){
-    state = 2;
-    //Serial.println(" Running");
-  }    
+  else if(stepFreq < 9) state = 2;
+  else if(stepFreq < 25) state  = 1;
+  else state = 0;
 }
 
 int AccelerometerAnalyzer::getState(){
